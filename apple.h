ce7b989268d00ea89dfd4df1ac46f9c19c315406
Logan Allen <loganallc@gmail.com>
2016-04-05 11:26:39 -0400

    Fix the colors, which were apparently off the whole time lol.

--- apple.h
+++ apple.h
@@ -1,6 +1,6 @@
 #ifndef APPLE_BITMAP_H
 #define APPLE_BITMAP_H
-extern const unsigned short apple[324];
+extern unsigned short apple[324];
 #define APPLE_WIDTH 18
 #define APPLE_HEIGHT 18
 #endif
--- handleDraw.c
+++ handleDraw.c
@@ -32,7 +32,19 @@ void playDraw(Game* game) {
     if (game->shouldDrawBackground) {
         fillScreen(game->backgroundColor);
         game->shouldDrawBackground = 0;
-        delay(30);
+        unsigned short replaceColor = 0x0001f;
+        if (game->backgroundColor == GREEN) {
+            replaceColor = CYAN;
+        } else if (game->backgroundColor == BLACK) {
+            replaceColor = GREEN;
+        }
+
+        for (int i = 0; i < APPLE_HEIGHT * APPLE_WIDTH; i++) {
+            if (apple[i] == replaceColor) {
+                apple[i] = game->backgroundColor;
+            }
+        }
+        delay(20);
     }
 
     // draw over old object locations with background color
@@ -79,30 +91,38 @@ void playDraw(Game* game) {
         }
     }
 
-    // draw objects
-    for(int i=0; i < game->enemyCount; i++) {
-        cur = game->objs + i;
-        drawRect(cur->row, cur->col, cur->size, cur->size, cur->color);
-        game->oldobjs[i] = game->objs[i];
-    }
-
-    // draw ship
-    cur = &game->ship;
-    // drawRect(cur->row, cur->col, cur->size, cur->size, cur->color);
-    drawImage3(cur->row, cur->col, APPLE_WIDTH, APPLE_HEIGHT, apple);
-    game->oldship = game->ship;
-
     // draw text for lives
     drawRect(1, 1, 8, 16, game->backgroundColor);
     char lives[3];
     sprintf(lives, "\x03%d", game->lives);
     drawString(1, 1, lives, WHITE);
 
+    // draw objects
+    for(int i=0; i < game->enemyCount; i++) {
+        cur = game->objs + i;
+        if (cur->type != DEAD) {
+            drawRect(cur->row, cur->col, cur->size, cur->size, cur->color);
+            game->oldobjs[i] = game->objs[i];
+        }
+    }
+
+    // draw ship
+    cur = &game->ship;
+    drawImage3(cur->row, cur->col, APPLE_WIDTH, APPLE_HEIGHT, apple);
+    game->oldship = game->ship;
+
 }
 
 void gameoverDraw(Game* game) {
     if (game->shouldDrawBackground) {
         fillScreen(BLUE);
+        unsigned short replaceColor = game->backgroundColor;
+
+        for (int i = 0; i < APPLE_HEIGHT * APPLE_WIDTH; i++) {
+            if (apple[i] == replaceColor) {
+                apple[i] = CYAN;
+            }
+        }
     }
 }
 
